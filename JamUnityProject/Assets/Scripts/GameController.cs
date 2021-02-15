e9bfd7720f09f9cf9f88d7af5870b35c55ada4be
Cinnazeyy <33779112+Cinnazeyy@users.noreply.github.com>
2021-02-15 16:59:58 +0100

    added some camera stuff

--- JamUnityProject/Assets/Scripts/GameController.cs
+++ JamUnityProject/Assets/Scripts/GameController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> playerShips = new List<GameObject>();
-    private int shipIndex = 0;
+    public int shipIndex = 0;
+    public CinemachineVirtualCamera cinemachine;
 
     // Update is called once per frame
     void Update()
@@ -28,5 +30,12 @@ public class GameController : MonoBehaviour
 
         playerShips[shipIndex].GetComponent<PlayerController>().isActive = true;
         Debug.Log("switched Ship");
+        SwitchCamera();
+    }
+
+    private void SwitchCamera()
+    {
+        cinemachine.Follow = playerShips[shipIndex].transform;
+        cinemachine.LookAt = playerShips[shipIndex].transform;
     }
 }
