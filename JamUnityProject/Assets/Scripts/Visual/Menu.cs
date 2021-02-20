b9fb635e5a21c93f9ad45f295718444262bfe56c
Cinnazeyy <33779112+Cinnazeyy@users.noreply.github.com>
2021-02-20 19:29:11 +0100

    Added Menu

--- JamUnityProject/Assets/Scripts/Visual/Menu.cs
+++ JamUnityProject/Assets/Scripts/Visual/Menu.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject controlls;
+
     void Start()
     {
-        
+        if (Application.isEditor)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     void Update()
     {
-        
+        //Show / Close Menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menu.SetActive(!menu.activeSelf);
+
+            if (Application.isEditor)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+        //Quit Game
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+
+        //Close Controlls
+        if (Input.anyKeyDown)
+        {
+            if (controlls.activeSelf)
+            {
+                controlls.SetActive(false);
+            }
+        }
+
+        //Show Controlls
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            controlls.SetActive(true);
+            menu.SetActive(false);
+        }
     }
 }
