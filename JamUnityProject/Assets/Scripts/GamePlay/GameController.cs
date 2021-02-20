f0140f002f9b8eb2cf77087eb719f735420b5da9
jo_kil <jo.kil.plays@gmail.com>
2021-02-20 02:26:15 +0100

    Changed Input manager to support controllers

--- JamUnityProject/Assets/Scripts/GamePlay/GameController.cs
+++ JamUnityProject/Assets/Scripts/GamePlay/GameController.cs
@@ -19,7 +19,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab) && controlIsEnabled)
+        if (Input.GetButtonDown("Switch") && controlIsEnabled)
         {
             SwitchPlayer();
         }
